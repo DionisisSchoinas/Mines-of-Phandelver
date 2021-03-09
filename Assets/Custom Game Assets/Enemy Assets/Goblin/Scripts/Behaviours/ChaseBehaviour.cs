9bbbe80db9e889475b7e55ea3895250908a79e80
Schoinas Dionisis <schinasdionisis@gmail.com>
2021-03-09 15:21:50 +0200

    Readded base sounds

--- Assets/Custom Game Assets/Enemy Assets/Goblin/Scripts/Behaviours/ChaseBehaviour.cs
+++ Assets/Custom Game Assets/Enemy Assets/Goblin/Scripts/Behaviours/ChaseBehaviour.cs
@@ -28,8 +28,6 @@ public class ChaseBehaviour : StateMachineBehaviour
             aiScript.Stop();
             animator.SetBool("Engage", true);
         }
-
-
     }
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
