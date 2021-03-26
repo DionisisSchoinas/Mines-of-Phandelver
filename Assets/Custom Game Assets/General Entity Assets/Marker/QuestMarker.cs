5288efe4d092eaa5cddf6eda45949aba19f580c3
Schoinas Dionisis <schinasdionisis@gmail.com>
2021-03-26 21:56:30 +0200

    Added Quest Marker
    Custom Game Assets -> General Entity Assets -> Marker

---
+++ Assets/Custom Game Assets/General Entity Assets/Marker/QuestMarker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestMarker : MonoBehaviour
+{
+    public float rotationSpeed;
+    public float upDownSize;
+
+    private Transform objectToMove;
+
+    private void Awake()
+    {
+        objectToMove = gameObject.GetComponentsInChildren<Transform>()[1];
+    }
+
+    private void FixedUpdate()
+    {
+        objectToMove.RotateAround(transform.position, transform.up, Time.deltaTime * rotationSpeed);
+
+        objectToMove.position += new Vector3(0f, Time.deltaTime * Mathf.Sin(Time.time) * upDownSize, 0f);
+    }
+}
