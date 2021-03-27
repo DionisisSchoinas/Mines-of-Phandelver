bae55286844cf9fe78caeecfc2b6f60f887cea38
Schoinas Dionisis <schinasdionisis@gmail.com>
2021-03-27 22:59:25 +0200

    Almost a scene

---
+++ Assets/milosCamerascript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class milosCamerascript : MonoBehaviour
+{
+    public Transform playerPosition;
+    public Vector3 offset;
+    public float smoothSpeed = 2f;
+
+
+
+    bool isWalled;
+
+    void FixedUpdate()
+    {
+
+
+
+        Vector3 targerPosition = playerPosition.position + offset; ;
+
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targerPosition, smoothSpeed * Time.deltaTime);
+
+        transform.position = smoothedPosition;
+
+        transform.LookAt(playerPosition);
+
+    }
+}
